71c83957633dd2d6ccece09b9465da12bfedf5a5
zhangjipeng <2652512560@qq.com>
2019-12-26 19:28:11 +0800

    首页增加了样式

---
+++ components/home/selectcondition/Selectcondition.js
@@ -0,0 +1,23 @@
+// components/home/selectcondition/Selectcondition.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+	itemList:[{title:'减免运费',val:0},{title:'高优门店',val:1},{title:'叠加红包',val:2},{title:'门店类型',val:3}]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/home/sort/SortTag.js
@@ -0,0 +1,23 @@
+// components/home/sort/SortTag.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+	sortList:[{title:"综合排序",key:0},{title:"销量",key:1},{title:"距离",key:2}]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
