0a3081c96a2b2240467bdb943c22d89e156cb189
zhangjipeng <2652512560@qq.com>
2019-12-30 17:39:24 +0800

    增加了发现页面的其他4个页面

--- components/home/search/Search.js
+++ components/home/search/Search.js
@@ -11,13 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+	searchContent:'搜索附近的商家商品'
   },
-
   /**
    * 组件的方法列表
    */
   methods: {
-
+	enterSearch(){
+		// 进入搜索页面
+		wx.navigateTo({
+			url:'/pages/childpages/home/search/search?data='+this.data.searchContent,
+			success:function(){
+				
+			}
+			
+		})
+	}
   }
 })
--- pages/childpages/home/search/search.js
+++ pages/childpages/home/search/search.js
@@ -1,4 +1,4 @@
-// pages/childpages/attention/attention.js
+// pages/childpages/home/search/search.js
 Page({
 
   /**
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+	  console.log(options);
   },
 
   /**
