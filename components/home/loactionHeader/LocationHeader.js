9604bad2f08150966cafa8a64e008637071b6363
起个名字真难 <oncwnuFR3YYnj5gMWScIizyy2yDQ@git.weixin.qq.com>
2020-03-23 23:18:29 +0800

    提交代码

--- components/home/loactionHeader/LocationHeader.js
+++ components/home/loactionHeader/LocationHeader.js
@@ -4,14 +4,21 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+	addressName:{
+		type:Object,
+		observer:function(newVal){
+			this.setData({
+				addressName:newVal
+			})
+		}
+	}
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+	addressName:{}
   },
 
   /**
